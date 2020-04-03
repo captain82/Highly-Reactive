447ad627ea0712f9b92fe62a2c40199d07a14ca9
captain82 <nktkumar82@gmail.com>
2020-04-03 20:04:19 +0530

    feat:Add retrofit in module

--- app/src/main/java/com/example/rxjava/injection/module/NetworkModule.kt
+++ app/src/main/java/com/example/rxjava/injection/module/NetworkModule.kt
@@ -1,9 +1,13 @@
 package com.example.rxjava.injection.module
 
 import com.example.rxjava.network.PostApi
+import com.example.rxjava.utils.Constants
 import dagger.Module
 import dagger.Provides
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -11,7 +15,16 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    internal fun providePostApi(retrofit: Retrofit):PostApi =
+    internal fun providePostApi(retrofit: Retrofit): PostApi =
         retrofit.create(PostApi::class.java)
+
+    @Provides
+    @Singleton
+    internal fun provideRetrofitInterface(): Retrofit =
+        Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.
+                createWithScheduler(Schedulers.io()))
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
 
 }
